91a409a5a6d9c6b90ee3e7efc6d38fc6bca2bf56
Prabhav Khandelwal <prabhav@alright.studio>
2021-09-16 17:17:30 -0400

    mobile footerrrr

--- assets/js/production.js
+++ assets/js/production.js
@@ -5,12 +5,17 @@
 
 var main = function() {
 	$('.nav-toggle').click(function() {
-			console.log('icon clikced');
-			$('nav.mobile').toggleClass('hidden');
+		console.log('icon clikced');
+		$('nav.mobile').toggleClass('hidden');
 	});
 	$('img.cross').click(function() {
-			console.log('icon clikced');
-			$('nav.mobile').toggleClass('hidden');
+		console.log('icon clikced');
+		$('nav.mobile').toggleClass('hidden');
+	});
+
+	$('footer.footer nav li:first-of-type').click(function() {
+		$(this).siblings('.expanding').toggleClass("show");
+		$(this).toggleClass('expanded');
 	});
 	
 	$(window).on("scroll", function() {
